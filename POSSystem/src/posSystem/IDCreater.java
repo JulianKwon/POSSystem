4788fc357dc00d42ff4f11075306b844a91e8be0
Julian <Julian@Julian-PC>
2016-06-05 13:52:27 +0900

    V_1.1.1

--- POSSystem/src/posSystem/IDCreater.java
+++ POSSystem/src/posSystem/IDCreater.java
@@ -1,4 +1,5 @@
 package posSystem;
+
 import java.util.*;
 
 public class IDCreater
@@ -6,16 +7,31 @@ public class IDCreater
 	private int randomid;
 	private Random r;
 	private ArrayList<Integer> IDs;
-	
+
 	public int IDCreate()
 	{
 		r = new Random();
 		IDs = new ArrayList<Integer>();
+
+		String str = "";
+		int d = 0;
+		for (int i = 1; i <= 4; i++)
+		{
+			d = r.nextInt(9);
+			str = str + Integer.toString(d);
+		}
+		randomid = Integer.parseInt(str);
 		
-		randomid = r.nextInt(9998) + 1;
+		while (IDs.contains(randomid))
+		{
+			for (int i = 1; i <= 4; i++)
+			{
+				d = r.nextInt(9);
+				str = str + Integer.toString(d);
+			}
+			randomid = Integer.parseInt(str);
+		}
 		
-		while(IDs.contains(randomid))
-			randomid = r.nextInt(9998) + 1;
 		IDs.add(randomid);
 		return randomid;
 	}
--- POSSystem/src/posSystem/Openfile.java
+++ POSSystem/src/posSystem/Openfile.java
@@ -8,82 +8,129 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.StringTokenizer;
 
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 public class Openfile
 {
 	private File input;
 	private static Connection dbTest;
+	private static JFrame open = new JFrame();
+	private static PreparedStatement stmt;
+	private static ResultSet rs;
+	private static IDCreater idc = new IDCreater();
+	private static StringTokenizer st;
 
-	public Openfile(File in)
+	public Openfile()
 	{
-		input = in;
-		System.out.println("저장경로 : " + input.toString());
-		read();
-	}
+		String sql = null;
 
-	void read()
-	{
-		IDCreater idc = new IDCreater();
-		int num = 0;
-		try
+		// connectdb
+		// initiate db
+
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setFileFilter(new FileNameExtensionFilter("텍스트", "txt"));
+		fileChooser.setCurrentDirectory(new File(System.getProperty("user.home") + "//" + "Desktop"));
+
+		int result = fileChooser.showOpenDialog(open);
+
+		if (result == JFileChooser.APPROVE_OPTION)
 		{
-			FileReader fileReader = new FileReader(input);
-			BufferedReader reader = new BufferedReader(fileReader);
-			String line = null;
-			while ((line = reader.readLine()) != null)
+			input = fileChooser.getSelectedFile();
+
+			System.out.println("저장경로 : " + input.toString());
+			int mod = 0;
+
+			try
 			{
-				if (isStringDouble(line))
+
+				FileReader fileReader = new FileReader(input);
+				BufferedReader reader = new BufferedReader(fileReader);
+				String line = null;
+				while ((line = reader.readLine()) != null)
 				{
-					if(num == 0)
+
+					if (line.length() <= 2)
+						mod++;
+
+					if (mod == 1)
 					{
-						String clientname = null, rate = null;
-						int birthday = 0, phonenumber = 0, ID = 0;
-						
-						for (int i = 0 ; i < Integer.parseInt(line); i++)
+						st = new StringTokenizer(line, "\t");
+						int clientid = idc.IDCreate(), totalpay = 0;
+
+						String[] str1 = new String[st.countTokens()];
+
+						for (int i = 0; i < str1.length; i++)
+							str1[i] = st.nextToken();
+						if (str1[3].equals("Gold"))
+							totalpay = 1000000;
+						else if (str1[3].equals("Silver"))
+							totalpay = 500000;
+						else if (str1[3].equals("Bronze"))
+							totalpay = 100000;
+
+						if (str1.length >= 2)
 						{
-							int count = 3;
-							line = reader.readLine();
-							StringTokenizer st = new StringTokenizer(line, "\t");
-							while(st.hasMoreTokens())
-							{
-								if(count == 3)
-									clientname = st.nextToken();
-								else if (count == 2)
-									birthday = Integer.parseInt(st.nextToken());
-								else if (count == 1)
-									phonenumber = Integer.parseInt(st.nextToken());
-								else
-									rate = st.nextToken();
-								count--;
-							}
-							ID = idc.IDCreate();
-							
-							String sqlStr = "insert into client values(" + ID + ", " + clientname + ", " + birthday + ", " + phonenumber + ", "
-									+ rate + ")";
-							PreparedStatement stmt = dbTest.prepareStatement(sqlStr);
-							ResultSet rs = stmt.executeQuery();
-							rs.next();
+							sql = "insert into customer(id, name, birth, phone, rank, totalspent) values(" + clientid
+									+ ", '" + str1[0] + "', " + str1[1] + "," + str1[2] + ",'" + str1[3] + "',"
+									+ totalpay + ")";
+
+							stmt = dbTest.prepareStatement(sql);
+							rs = stmt.executeQuery();
+							rs.close();
+							stmt.close();
 						}
-						
+
+					}
+					if (mod == 2)
+					{
+						st = new StringTokenizer(line, "\t");
+						int employeeid = idc.IDCreate();
+
+						String[] str2 = new String[st.countTokens()];
+
+						for (int i = 0; i < str2.length; i++)
+							str2[i] = st.nextToken();
+
+						if (str2.length > 1)
+						{
+							sql = "insert into employee(name, role, id, totalsell) values('" + str2[0] + "', '"
+									+ str2[1] + "'," + employeeid + ", 0)";
+							stmt = dbTest.prepareStatement(sql);
+							rs = stmt.executeQuery();
+							rs.close();
+							stmt.close();
+						}
+					}
+
+					if (mod == 3)
+					{
+						st = new StringTokenizer(line, "\t");
+						int menuid = idc.IDCreate();
+
+						String[] str3 = new String[st.countTokens()];
+
+						for (int i = 0; i < str3.length; i++)
+							str3[i] = st.nextToken();
+
+						if (str3.length >= 2)
+						{
+							sql = "insert into menu values(" + menuid + ", '" + str3[0] + "', " + str3[1] + ")";
+							stmt = dbTest.prepareStatement(sql);
+							rs = stmt.executeQuery();
+							rs.close();
+							stmt.close();
+						}
 					}
 				}
+
+				// read line then insert number
+
+				reader.close();
+			} catch (Exception ex)
+			{
 			}
-			// read line then insert number
-
-			reader.close();
-		} catch (Exception ex)
-		{
-		}
-	}
-
-	public static boolean isStringDouble(String s)
-	{
-		try
-		{
-			Double.parseDouble(s);
-			return true;
-		} catch (NumberFormatException e)
-		{
-			return false;
 		}
 	}
 }
